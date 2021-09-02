ce1fda9664f25e4d9b2170cac9d0dd255ea7953b
Christian Saavedra <christian-saavedra@hotmail.com>
2021-09-02 14:03:03 -0400

    header fixes

--- src/UIComponents/Header.js
+++ src/UIComponents/Header.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "../App.css";
 import { withRouter } from "react-router";
-import FCStatistics from "../FreeCompany/FCStatistics";
-import MainMenu from "./MainMenu";
-import EmptyPage from "../EmptyPage";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export const Header = (props) => {
@@ -13,12 +10,12 @@ export const Header = (props) => {
         <Link to="/">
           <h1 class="header-text">Home</h1>
         </Link>
-        <Link class="header-text" to="/empty">
+        {/* <Link class="header-text" to="/empty">
           <h1 class="header-text">Under Construction</h1>
         </Link>
         <Link class="header-text" to="/empty">
           <h1 class="header-text">Under Construction</h1>
-        </Link>
+        </Link> */}
       </div>
     );
   } else {
