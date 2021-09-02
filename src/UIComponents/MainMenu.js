d08918db09c08336c4dc4c66d3286b2f9071aef9
Christian Saavedra <christian-saavedra@hotmail.com>
2021-09-02 13:56:16 -0400

    fixed bug

--- src/UIComponents/MainMenu.js
+++ src/UIComponents/MainMenu.js
@@ -1,13 +1,14 @@
 import React from "react";
+import FCStatistics from "../FreeCompany/FCStatistics";
 import "../App.css";
-import { BrowserRouter as Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export const MainMenu = () => {
   return (
-    <div className="main">
-      <Link className="card" to="/fc-statistics">FC Demographics</Link>
-      <Link className="card" to="/empty">Under Construction</Link>
-      <Link className="card" to="/empty">Under Construction</Link>
+    <div class="main">
+      <Link class="card" to="/fc-statistics">FC Demographics</Link>
+      <Link class="card" to="/empty">Under Construction</Link>
+      <Link class="card" to="/empty">Under Construction</Link>
     </div>
   );
 };
